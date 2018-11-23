c384cca1a73ddcd8473c101e6ffa45d5f3e8da0b
Brian Toll <briantoll@gmail.com>
2018-11-23 17:34:24 -0500

    through lesson 64

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -36,20 +36,56 @@
 
 // Object.create
 
-var personProto = {
-  calculateAge: function() {
-    console.log(2018 - this.yearOfBirth);
-  }
-};
+// var personProto = {
+//   calculateAge: function() {
+//     console.log(2018 - this.yearOfBirth);
+//   }
+// };
+//
+// var brian = Object.create(personProto);
+// brian.name = 'Brian';
+// brian.yearOfBirth = 1984;
+// brian.job = 'manager';
+//
+// var jane = Object.create(personProto,
+// {
+//   name: { value: 'Jane' },
+//   yearOfBirth: { value: 1969 },
+//   job: { value: 'designer' }
+// });
 
-var brian = Object.create(personProto);
-brian.name = 'Brian';
-brian.yearOfBirth = 1984;
-brian.job = 'manager';
 
-var jane = Object.create(personProto,
-{
-  name: { value: 'Jane' },
-  yearOfBirth: { value: 1969 },
-  job: { value: 'designer' }
-});
+
+// Primitives vs objects
+
+// var a = 23;
+// var b = a;
+// a = 46;
+// console.log(a);
+// console.log(b); //mutation doesn't persist to var b
+//
+// var obj1 = {
+//   name: 'Brian',
+//   age: 34
+// }
+// var obj2 = obj1;
+// obj1.age = 35;
+// console.log(obj1.age);
+// console.log(obj2.age); //mutation persists to obj2
+//
+// // Functions
+// var age = 26;
+// var obj = {
+//   name: 'Katerina',
+//   city: 'Athens'
+// }
+//
+// function change(a, b) {
+//   a = 24;
+//   b.city = 'Philadelphia'
+// }
+//
+// change(age, obj);
+//
+// console.log(age);
+// console.log(obj.city);
