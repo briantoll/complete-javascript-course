2266b75d1c92446a7630937406413112c53a37cc
Brian Toll <briantoll@gmail.com>
2018-11-25 10:02:24 -0500

    through lesson 67

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -89,3 +89,83 @@
 //
 // console.log(age);
 // console.log(obj.city);
+
+
+
+//Passing functions as arguments
+
+// var years = [1990, 1965, 1937, 2005, 1998];
+//
+// function arrayCalc(arr, fn) {
+//   var arrRes = [];
+//   for (var i = 0; i < arr.length; i++) {
+//     arrRes.push(fn(arr[i]));
+//   }
+//   return arrRes;
+// }
+//
+// function calculateAge(el) {
+//   return 2018 - el;
+// }
+//
+// function isFullAge(el) {
+//   return el >= 18;
+// }
+//
+// function maxHeartRate(el) {
+//   if (el >= 18 && el <= 81) {
+//     return Math.round(206.9 - (0.67 * el));
+//   } else {
+//     return -1;
+//   }
+// }
+//
+// var ages = arrayCalc(years, calculateAge);
+// var fullAges = arrayCalc(ages, isFullAge);
+// var rates = arrayCalc(ages, maxHeartRate);
+// console.log(ages);
+// console.log(rates);
+
+//Functions returings functions
+
+// function interviewQuestion(job) {
+//   if (job === 'designer') {
+//     return function(name) {
+//       console.log(name + ', can you please explain what UX design is?');
+//     }
+//   } else if (job === 'teacher') {
+//     return function(name) {
+//       console.log('What subject do you teach, ' + name + '?');
+//     }
+//   } else {
+//     return function(name) {
+//       console.log('Hello ' + name + ', what do you do?');
+//     }
+//   }
+// }
+//
+// var teacherQuestion = interviewQuestion('teacher');
+// var designerQuestion = interviewQuestion('designer');
+//
+// teacherQuestion('John');
+// designerQuestion('Brian');
+//
+// var teacherQuestion = interviewQuestion('teacher')('Katerina');
+
+//IIFE
+
+// function game() {
+//   var score = Math.random() * 10;
+//   console.log(score >= 5);
+// }
+// game();
+
+(function() {
+  var score = Math.random() * 10;
+  console.log(score >= 5);
+})();
+
+(function(goodLuck) {
+  var score = Math.random() * 10;
+  console.log(score >= 5 - goodLuck);
+})(4);
