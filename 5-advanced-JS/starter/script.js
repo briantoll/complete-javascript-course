b4c7b3aa5ebab97b8bb91487fd67d6e3d1b1abb2
Brian Toll <briantoll@gmail.com>
2018-11-26 13:21:28 -0500

    through lesson 69

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -160,12 +160,49 @@
 // }
 // game();
 
-(function() {
-  var score = Math.random() * 10;
-  console.log(score >= 5);
-})();
+// (function() {
+//   var score = Math.random() * 10;
+//   console.log(score >= 5);
+// })();
+//
+// (function(goodLuck) {
+//   var score = Math.random() * 10;
+//   console.log(score >= 5 - goodLuck);
+// })(4);
 
-(function(goodLuck) {
-  var score = Math.random() * 10;
-  console.log(score >= 5 - goodLuck);
-})(4);
+// function retirement(retirementAge) {
+//   var a = ' years left until retirement.'
+//   return function(yearOfBirth) {
+//     var age = 2018 - yearOfBirth;
+//     console.log((retirementAge - age) + a);
+//   }
+// }
+//
+// var retirementUS = retirement(66);
+// var retirementGermany = retirement(65);
+// var retirementIceland = retirement(67);
+//
+// retirementUS(1984);
+// retirementGermany(1984);
+// retirementIceland(1984);
+//retirement(66)(1984);
+
+function interviewQuestion(job) {
+  return function(name) {
+    if (job === 'designer') {
+      console.log(name + ', can you please explain what UX design is?');
+    } else if (job === 'teacher') {
+      console.log('What subject do you teach, ' + name + '?');
+    } else {
+      console.log('Hello ' + name + ', what do you do?');
+    }
+  }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+
+teacherQuestion('John');
+designerQuestion('Brian');
+
+var teacherQuestion = interviewQuestion('teacher')('Katerina');
