8dfe93074e69c17e69df761c31d22fbba5f2cd86
Brian Toll <briantoll@fastmail.com>
2020-04-27 23:17:43 -0400

    through lecture 92

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -59,6 +59,21 @@ var budgetController = (function() {
 
     },
 
+    deleteItem: function(type, id) {
+      var ids, index;
+
+      var ids = data.allItems[type].map(function(current){
+        return current.id;
+      });
+
+      index = ids.indexOf(id);
+
+      if (index !== -1) {
+        data.allItems[type].splice(index, 1);
+      }
+
+    },
+
     calculateBudget: function() {
 
       // calculate total income and expenses
@@ -141,6 +156,13 @@ var UIController = (function() {
       document.querySelector(element).insertAdjacentHTML('beforeend', newHTML);
     },
 
+    deleteListItem: function (selectorID) {
+      
+      var element = document.getElementById(selectorID);
+      element.parentNode.removeChild(element);
+
+    },
+
     clearFields: function() {
       var fields, fieldsArray;
       
@@ -156,8 +178,6 @@ var UIController = (function() {
     },
 
     displayBudget: function(obj) {
-
-      console.log('obj.budget = ' + obj.budget);
 
       document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
       document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalIncome;
@@ -231,7 +251,7 @@ var controller = (function(budgetCtrl, UICtrl) {
   }
 
   var ctrlDeleteItem = function(event) {
-    var itemID, splitID, type, ID;
+    var itemID, splitID, type, id;
 
     //this is relying on the DOM structure being hard coded - not a best practice
     itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
@@ -239,15 +259,16 @@ var controller = (function(budgetCtrl, UICtrl) {
 
       splitID = itemID.split('-');
       type = splitID[0];
-      ID = splitID[1];
+      id = parseInt(splitID[1]);
 
       // 1. delete the item from the data structure
+      budgetController.deleteItem(type, id);
 
       // 2. delete the item from the UI
-
+      UIController.deleteListItem(itemID);
+      
       // 3. updateBudget
       updateBudget();
-
 
     }
 
