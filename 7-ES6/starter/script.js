8d60880752e987abbbc6917e3e97bc27ef57bb17
Brian Toll <briantoll@fastmail.com>
2020-04-29 08:44:11 -0400

    through lecture 105

---
+++ 7-ES6/starter/script.js
@@ -0,0 +1,80 @@
+// Lecture: let & const
+
+// ES5
+// var name5 = 'Brian Toll';
+// var age5 = '36';
+// name5 = 'Brian David Toll';
+// console.log(name5);
+
+// // ES6
+// const name6 = 'Brian Toll';
+// let age6 = 36;
+// //name6 = 'Brian David Toll';
+// console.log(name6);
+
+//ES5
+// function driversLicence5(passedTest) {
+
+//     if (passedTest) {
+//         var firstName = 'Brian';
+//         var yearOfBirth = 1984;
+
+//         console.log(firstName + yearOfBirth);
+//     }
+
+// }
+
+//driversLicence5(true);
+
+//ES6
+// function driversLicence6(passedTest) {
+
+//     //console.log(firstName);
+//     let firstName;
+//     const yearOfBirth = 1984;
+
+//     if (passedTest) {
+//         firstName = 'Brian';
+//         console.log(firstName + yearOfBirth);
+//     }
+
+// }
+
+// driversLicence6(true);
+
+// let i = 23;
+
+// for (let i = 0; i < 5; i++) {
+
+//     console.log(i);
+
+// }
+
+// console.log(i);
+
+// let and const are block scoped
+// var is function scope
+
+
+
+
+// Lecture: Blocks and IIFEs
+
+//ES6
+{
+    const a = 1;
+    let b = 2;
+    
+    // Not Block scoped so it can be accessed from outside the block
+    var c = 3;
+}
+
+//console.log(a + ' ' + b)
+console.log(c);
+
+// ES5 IIFE
+(function() {
+    var c = 3;
+})();
+
+//console.log(c);
