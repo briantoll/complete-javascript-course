8b095c18e9527f8247880f22d08a78086f1d8c12
Brian Toll <briantoll@fastmail.com>
2020-04-30 11:20:59 -0400

    through lecture 108

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -112,29 +112,102 @@
 
 //Lecutre: Arrow Functions
 
-const years = [1984, 1992, 1993, 1996];
+// const years = [1984, 1992, 1993, 1996];
 
-//ES5
-var ages5 = years.map(function(el) {
-    return 2020 - el;
-});
-console.log(ages5);
+// //ES5
+// var ages5 = years.map(function(el) {
+//     return 2020 - el;
+// });
+// console.log(ages5);
 
 
-//ES6
-let ages6 = years.map(el => 2020 - el);
-console.log(ages6);
+// //ES6
+// let ages6 = years.map(el => 2020 - el);
+// console.log(ages6);
 
-ages6 = years.map((el, index) => `Age element ${index+1}: ${2020-el}.`);
-console.log(ages6);
+// ages6 = years.map((el, index) => `Age element ${index+1}: ${2020-el}.`);
+// console.log(ages6);
 
-ages6 = years.map((el, index) => {
-    const now = new Date().getFullYear();
-    const age = now - el;
-    return `Age element ${index+1}: ${age}.`
-});
-console.log(ages6);
+// ages6 = years.map((el, index) => {
+//     const now = new Date().getFullYear();
+//     const age = now - el;
+//     return `Age element ${index+1}: ${age}.`
+// });
+// console.log(ages6);
 
 
 
 //Lecture: Lexical 'this' Keyword
+
+//ES5 
+// var box5 = {
+//   color: 'green',
+//   position: 1,
+//   clickMe: function() {
+//     var self = this;
+//     document.querySelector('.green').addEventListener('click', function() {
+//       var str = 'This is a number ' + self.position + ' and it is ' + self.color;
+//       alert(str);
+//     });
+//   }
+// }
+
+//box5.clickMe();
+
+//ES6
+
+// const box6 = {
+//   color: 'blue',
+//   position: 2,
+//   clickMe: function() {
+//     document.querySelector('.blue').addEventListener('click', () => {
+//       var str = 'This is a number ' + this.position + ' and it is ' + this.color;
+//       alert(str);
+//     });
+//   }
+// }
+
+//box6.clickMe();
+
+// const box66 = {
+//   color: 'blue',
+//   position: 2,
+//   clickMe: () => { // another arrow function here will break it since i changes the lexical
+//     // this scope to the global document
+//     document.querySelector('.blue').addEventListener('click', () => {
+//       var str = 'This is a number ' + this.position + ' and it is ' + this.color;
+//       alert(str);
+//     });
+//   }
+// }
+
+// box66.clickMe();
+
+
+function Person(name) {
+  this.name = name;
+}
+
+//ES5
+Person.prototype.myFriends5 = function(friends) {
+
+  var arr = friends.map(function(el) {
+    return this.name + ' is friends with ' + el;
+  }.bind(this));
+
+  console.log(arr);
+
+}
+
+var friends = ['Katerina', 'Julia', 'Chris'];
+new Person('Brian').myFriends5(friends);
+
+//ES6
+Person.prototype.myFriends6 = function(friends) {
+
+  var arr = friends.map(el => `${this.name} is friends with ${el}`);
+  console.log(arr);
+
+}
+
+new Person('Brian').myFriends6(friends);
