5351ed9d40686f1743ff0251f7cfd0bd680ab60f
Brian Toll <briantoll@fastmail.com>
2020-05-01 18:42:10 -0400

    through lecture 110

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -246,3 +246,54 @@
 // console.log(age);
 // console.log(retirement);
 
+
+// Lecture: Arrays
+
+const boxes = document.querySelectorAll('.box');
+
+//ES5
+
+// var boxesArr5 = Array.prototype.slice.call(boxes);
+
+// boxesArr5.forEach(function(cur) {
+//     cur.style.backgroundColor = 'dodgerblue';
+// });
+
+//ES6
+Array.from(boxes).forEach(cur => cur.style.backgroundColor = 'dodgerblue');
+
+
+//ES5
+// for(var i = 0; i < boxesArr5.length; i++) {
+
+//     if(boxesArr5[i].className === 'box blue') {
+//         continue;
+//     }
+
+//     boxesArr5[i].textContent = 'I changed to blue!';
+// }
+
+//ES6
+for (const cur of Array.from(boxes)) {
+    if (cur.className.includes('blue')) {
+        continue;
+    }
+    cur.textContent = 'I changed to blue!';
+}
+
+
+//ES5
+var ages = [12, 17, 8, 21, 14, 11];
+
+// var fullAge = ages.map(function(cur) {
+//     return cur >= 18;
+// });
+// console.log(fullAge);
+
+// console.log(fullAge.indexOf(true));
+// console.log(ages[fullAge.indexOf(true)]);
+
+
+//ES6
+console.log(ages.findIndex(cur => cur >= 18));
+console.log(ages.find(cur => cur >= 18));
