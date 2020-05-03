0fd99c5fb3855a6a6276c7091e4871160f5ba97d
Brian Toll <briantoll@fastmail.com>
2020-05-02 20:30:54 -0400

    through lecture 118

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -64,7 +64,7 @@
 // {
 //     const a = 1;
 //     let b = 2;
-    
+
 //     // Not Block scoped so it can be accessed from outside the block
 //     var c = 3;
 // }
@@ -386,10 +386,10 @@
 
 //ES5
 // function SmithPerson(firstName, yearOfBirth, lastName, nationality){
-    
+
 //     lastName === undefined ? lastName = 'Smith' : lastName;
 //     nationality === undefined ? nationality = 'american' : nationality = nationality;
-    
+
 //     this.firstName = firstName;
 //     this.lastname = lastName;
 //     this.yearOfBirth = yearOfBirth;
@@ -447,7 +447,7 @@
 //     if (typeof(key) === 'number') {
 //         console.log(`Answer ${key}: ${value}`);
 //     }
-    
+
 // }
 
 // const ans = parseInt(prompt('Write the correct answer'));
@@ -533,35 +533,157 @@
 
 
 // //ES6
-class Person6 {
-    constructor (name, yearOfBirth, job) {
+// class Person6 {
+//     constructor (name, yearOfBirth, job) {
+//         this.name = name;
+//         this.yearOfBirth = yearOfBirth;
+//         this.job = job;
+//     }
+
+//     calculateAge() {
+//         var age = new Date().getFullYear() - this.yearOfBirth;
+//         console.log(age);
+//     }
+
+// }
+
+// class Athlete6 extends Person6 {
+//     constructor(name, yearOfBirth, job, olympicGames, medals) {
+//         super(name, yearOfBirth, job);
+//         this.olympicGames = olympicGames;
+//         this.medals = medals;
+//     }
+
+//     wonMedal() {
+//         this.medals++;
+//         console.log(this.medals);
+//     }
+
+// }
+
+// const brianAthlete6 = new Athlete6('Brian', 1984, 'basketball', 4, 4);
+
+// brianAthlete6.calculateAge();
+// brianAthlete6.wonMedal();
+
+
+
+/////////////////////////////////
+// CODING CHALLENGE
+
+/*
+
+Suppose that you're working in a small town administration, and you're in charge of two town elements:
+1. Parks
+2. Streets
+
+It's a very small town, so right now there are only 3 parks and 4 streets. All parks and streets have a name and a build year.
+
+At an end-of-year meeting, your boss wants a final report with the following:
+1. Tree density of each park in the town (forumla: number of trees/park area)
+2. Average age of each town's park (forumla: sum of all ages/number of parks)
+3. The name of the park that has more than 1000 trees
+4. Total and average length of the town's streets
+5. Size classification of all streets: tiny/small/normal/big/huge. If the size is unknown, the default is normal
+
+All the report data should be printed to the console.
+
+HINT: Use some of the ES6 features: classes, subclasses, template strings, default parameters, maps, arrow functions, destructuring, etc.
+
+*/
+
+class Element {
+    constructor(name, buildYear) {
         this.name = name;
-        this.yearOfBirth = yearOfBirth;
-        this.job = job;
+        this.buildYear = buildYear;
     }
-
-    calculateAge() {
-        var age = new Date().getFullYear() - this.yearOfBirth;
-        console.log(age);
-    }
-
 }
 
-class Athlete6 extends Person6 {
-    constructor(name, yearOfBirth, job, olympicGames, medals) {
-        super(name, yearOfBirth, job);
-        this.olympicGames = olympicGames;
-        this.medals = medals;
+class Park extends Element {
+    constructor(name, buildYear, area, numTrees) {
+
+        super(name, buildYear);
+        this.area = area; //km2
+        this.numTrees = numTrees;
+
     }
 
-    wonMedal() {
-        this.medals++;
-        console.log(this.medals);
+    treeDensity() {
+        const density = this.numTrees / this.area;
+        console.log(`${this.name} has a tree density of ${density} trees per square km.`)
     }
-
 }
 
-const brianAthlete6 = new Athlete6('Brian', 1984, 'basketball', 4, 4);
+class Street extends Element {
 
-brianAthlete6.calculateAge();
-brianAthlete6.wonMedal();
+    constructor(name, buildYear, length, size = 3) {
+        super(name, buildYear);
+        this.length = length; //km
+        this.size = size;
+    }
+
+    classifyStreet() {
+        const classification = new Map();
+        classification.set(1, 'tiny');
+        classification.set(2, 'small');
+        classification.set(3, 'normal');
+        classification.set(4, 'big');
+        classification.set(4, 'huge');
+        console.log(`${this.name}, built in ${this.buildYear}, is a ${classification.get(this.size)} street.`)
+    }
+}
+
+
+const allParks = [new Park('Green Park', 1987, 0.2, 215),
+    new Park('National Park', 1894, 2.9, 3541),
+    new Park('Oak Park', 1953, 0.4, 949)
+];
+
+const allStreets = [new Street('Ocean Avenue', 1999, 1.1, 4),
+    new Street('Evergreen Street', 2008, 2.7, 2),
+    new Street('4th Street', 2015, 0.8),
+    new Street('Sunset Boulevard', 1982, 2.5, 5)
+];
+
+function calc(arr) {
+    
+    //first use of reduce to sum up the total of an array
+    const sum = arr.reduce((prev, cur, index) => prev + cur, 0);
+    
+    return [sum, sum / arr.length];
+    
+}
+
+
+function reportParks(p) {
+    
+    console.log('-----PARKS REPORT-----');
+    
+    // Density
+    p.forEach(el => el.treeDensity());
+    
+    // Average age
+    const [totalAge, avgAge] = calc(p.map(el => new Date().getFullYear() - el.buildYear));
+    console.log(`Our ${p.length} parks have an average of ${avgAge} years.`);
+    
+    // Which park has more than 1000 trees
+    const i = p.map(el => el.numTrees).findIndex(el => el >= 1000);
+    console.log(`${p[i].name} has more than 1000 trees.`);
+    
+}
+
+
+function reportStreets(s) {
+    
+    console.log('-----STREETS REPORT-----');
+    
+    //Total and average length of the town's streets
+    const [totalLength, avgLength] = calc(s.map(el => el.length));
+    console.log(`Our ${s.length} streets have a total length of ${totalLength} km, with an average of ${avgLength} km.`);
+    
+    // CLassify sizes
+    s.forEach(el => el.classifyStreet());
+}
+
+reportParks(allParks);
+reportStreets(allStreets);
